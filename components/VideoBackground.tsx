81f293c9300990c1ffd99c5b2f7220e06248fd8e
Rick <rick030503@gmail.com>
2026-01-09 01:40:00 +0800

    Fix video path for GitHub Pages deployment

--- components/VideoBackground.tsx
+++ components/VideoBackground.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 export default function VideoBackground() {
+  const basePath = process.env.NODE_ENV === 'production' ? '/useful-toolbox' : '';
+  
   return (
     <div className="fixed inset-0 -z-10 overflow-hidden">
       <video
@@ -10,7 +12,7 @@ export default function VideoBackground() {
         playsInline
         className="absolute top-1/2 left-1/2 min-w-full min-h-full w-auto h-auto -translate-x-1/2 -translate-y-1/2 object-cover"
       >
-        <source src="/bg2.mp4" type="video/mp4" />
+        <source src={`${basePath}/bg2.mp4`} type="video/mp4" />
       </video>
       <div className="absolute inset-0 bg-black/20" />
     </div>
